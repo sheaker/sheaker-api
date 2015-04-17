0dc79a24dafda743edc18b60115339f64214fd9d
Tuxity <kevin.darcel@gmail.com>
2015-04-17 18:25:14 -0500

    Remove json_encode depth, fix charset

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -43,13 +43,17 @@ $app->register(new DoctrineServiceProvider(), [
             'dbname'   => 'client_' . $idClient,
             'host'     => $app['database.host'],
             'user'     => $app['database.user'],
-            'password' => $app['database.passwd']
+            'password' => $app['database.passwd'],
+            'charset'  => 'utf8',
+            'driverOptions' => array(1002 => 'SET NAMES utf8')
         ],
         'sheaker' => [
             'dbname'   => 'sheaker',
             'host'     => $app['database.host'],
             'user'     => $app['database.user'],
-            'password' => $app['database.passwd']
+            'password' => $app['database.passwd'],
+            'charset'  => 'utf8',
+            'driverOptions' => array(1002 => 'SET NAMES utf8')
         ]
     ]
 ]);
