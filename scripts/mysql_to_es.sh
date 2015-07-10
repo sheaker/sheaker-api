e23d4b486dfcefda6628d48dc34f6813c679e3fb
Tuxity <kevin.darcel@gmail.com>
2015-07-10 10:43:48 -0500

    Scripts/ESIndexing: Improve indexing script

--- scripts/mysql_to_es.sh
+++ scripts/mysql_to_es.sh
@@ -3,6 +3,27 @@
 lib=/Users/kevin/work/hexagone/sheaker-miscs/elasticsearch-jdbc-1.6.0.0/lib
 bin=/Users/kevin/work/hexagone/sheaker-miscs/elasticsearch-jdbc-1.6.0.0/bin
 
+printf 'Deleting index...'
+curl -silent -XDELETE 'localhost:9200/client_1' > /dev/null
+echo -e '\t\t\tOK'
+
+printf 'Creating mapping...'
+curl -silent -XPOST 'http://localhost:9200/client_1' -d '{
+    "mappings" : {
+        "checkin" : {
+            "_parent" : {
+              "type" : "user"
+            }
+        },
+        "payment" : {
+            "_parent" : {
+              "type" : "user"
+            }
+        }
+    }
+}' > /dev/null
+echo -e '\t\t\tOK'
+
 printf 'Importing users table...'
 echo '{
     "type" : "jdbc",
@@ -10,8 +31,8 @@ echo '{
         "url" : "jdbc:mysql://localhost:3306/client_1",
         "user" : "root",
         "password" : "",
-        "sql" : "select *, id as _id from users",
-        "index" : "users",
+        "sql" : "SELECT *, id AS _id FROM users",
+        "index" : "client_1",
         "type" : "user",
         "elasticsearch" : {
            "cluster" : "elasticsearch_brew",
@@ -33,8 +54,8 @@ echo '{
         "url" : "jdbc:mysql://localhost:3306/client_1",
         "user" : "root",
         "password" : "",
-        "sql" : "select *, id as _id from users_checkin",
-        "index" : "users_checkin",
+        "sql" : "SELECT *, id AS _id, user_id AS _parent FROM users_checkin",
+        "index" : "client_1",
         "type" : "checkin",
         "elasticsearch" : {
            "cluster" : "elasticsearch_brew",
@@ -56,8 +77,8 @@ echo '{
        "url" : "jdbc:mysql://localhost:3306/client_1",
        "user" : "root",
        "password" : "",
-       "sql" : "select *, id as _id from users_payments",
-       "index" : "users_payments",
+       "sql" : "SELECT *, id AS _id, user_id AS _parent FROM users_payments",
+       "index" : "client_1",
        "type" : "payment",
        "elasticsearch" : {
           "cluster" : "elasticsearch_brew",
