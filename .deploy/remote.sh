f8c3fa503aec8db924eb486eb37388b017a3b495
Tuxity <kevin.darcel@gmail.com>
2015-09-11 12:12:05 -0500

    Drone: Final remote deploy script

--- .deploy/remote.sh
+++ .deploy/remote.sh
@@ -1,20 +1,24 @@
-TEMP_DIR=/tmp/sheaker-back
-PROD_DIR=/var/www/sheaker.com/sheaker-back
+APP_NAME=sheaker-back
+
+TEMP_FILE=/tmp/$APP_NAME.tar.gz
+TEMP_DIR=/tmp/$APP_NAME
+PROD_DIR=/var/www/sheaker.com/$APP_NAME
 PROD_NAME=$(date +%s)
 
-#sudo /etc/init.d/nginx stop
+sudo /etc/init.d/nginx stop
 
 mkdir $PROD_DIR/$PROD_NAME
 cp -pr $TEMP_DIR $PROD_DIR/$PROD_NAME
 
 cd $PROD_DIR
 
-#unlink $PROD_DIR/current
-#ln -s $PROD_NAME current
+unlink $PROD_DIR/current
+ln -s $PROD_NAME current
 
 cd current/public
 ln -s ../../photos photos
 
-#sudo /etc/init.d/nginx start
+sudo /etc/init.d/nginx start
 
 rm -rf $TEMP_DIR
+rm -rf $TEMP_FILE
