37529543e8fa552fcfdb95822aa5fcc25a8385cd
Tuxity <kevin.darcel@gmail.com>
2015-09-11 16:16:32 -0500

    Drone: Final script

--- .deploy/remote.sh
+++ .deploy/remote.sh
@@ -5,12 +5,13 @@ TEMP_DIR=/tmp/$APP_NAME
 PROD_DIR=/var/www/sheaker.com/$APP_NAME
 PROD_NAME=$(date +%s)
 
-sudo /etc/init.d/nginx stop
-
-mkdir $PROD_DIR/$PROD_NAME
 cp -pr $TEMP_DIR $PROD_DIR/$PROD_NAME
 
 cd $PROD_DIR
+
+sudo chown ubuntu:www-data $PROD_NAME
+
+sudo /etc/init.d/nginx stop
 
 unlink $PROD_DIR/current
 ln -s $PROD_NAME current
