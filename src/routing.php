64a227a45acfa691142f1016da1d5677d15f76a4
Tuxity <kevin.darcel@gmail.com>
2015-10-14 09:00:52 -0500

    Create another before middleware and manually call it to avoid a f*cking if in the app before middleware

--- src/routing.php
+++ src/routing.php
@@ -3,78 +3,102 @@
 /**
  * Main routes
  */
-$app->get('/clients',                 'Sheaker\Controller\MainController::getClient');
+$app->get('/clients',                 'Sheaker\Controller\MainController::getClient')
+    ->before($fetchClient);
 $app->post('/clients',                'Sheaker\Controller\MainController::createClient');
 $app->put('/clients/index',           'Sheaker\Controller\MainController::indexClient')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->get('/infos',                   'Sheaker\Controller\MainController::getSheakerInfos');
 
 /**
  * Users routes
  */
-$app->post('/users/login',            'Sheaker\Controller\UserController::login');
-$app->post('/users/renew_token',      'Sheaker\Controller\UserController::renewToken');
+$app->post('/users/login',            'Sheaker\Controller\UserController::login')
+    ->before($fetchClient);
+$app->post('/users/renew_token',      'Sheaker\Controller\UserController::renewToken')
+    ->before($fetchClient)
+    ->before($checkToken);
 
 $app->get('/users',                   'Sheaker\Controller\UserController::getUsersList')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->get('/users/search',            'Sheaker\Controller\UserController::searchUsers')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->get('/users/{user_id}',         'Sheaker\Controller\UserController::getUser')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->post('/users',                  'Sheaker\Controller\UserController::addUser')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->put('/users/{user_id}',         'Sheaker\Controller\UserController::editUser')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->delete('/users/{user_id}',      'Sheaker\Controller\UserController::deleteUser')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 $app->get('/users/stats/active',      'Sheaker\Controller\UsersStatisticsController::getActiveUsers')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->get('/users/stats/new',         'Sheaker\Controller\UsersStatisticsController::getNewUsersFromDate')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 $app->get('/users/graph/new',         'Sheaker\Controller\UsersGraphicsController::getNewUsersFromDate')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->get('/users/graph/sex',         'Sheaker\Controller\UsersGraphicsController::getGenderRepartition')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 /**
  * Payments routes
  */
 $app->get('/payments/{payment_id}',     'Sheaker\Controller\PaymentController::getPayment')
     ->assert('payment_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 $app->get('/payments/stats/gains',      'Sheaker\Controller\PaymentsStatisticsController::getGainsFromDate')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 $app->get('/payments/graph/gains',      'Sheaker\Controller\PaymentsGraphicsController::getGains')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 // Payments by user
 $app->get('/users/{user_id}/payments',  'Sheaker\Controller\PaymentController::getPaymentsListByUser')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->post('/users/{user_id}/payments', 'Sheaker\Controller\PaymentController::addPayment')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 /**
  * Checkin routes
  */
 $app->get('/checkins/stats/new',        'Sheaker\Controller\CheckinsStatisticsController::getCheckinsFromDate')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 $app->get('/checkins/graph/new',        'Sheaker\Controller\CheckinsGraphicsController::getCheckinsFromDate')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 
 // Checkins by user
 $app->get('/users/{user_id}/checkins',  'Sheaker\Controller\CheckinController::getCheckinsListByUser')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
 $app->post('/users/{user_id}/checkins', 'Sheaker\Controller\CheckinController::addCheckin')
     ->assert('user_id', '\d+')
-    ->before($beforeCheckToken);
+    ->before($fetchClient)
+    ->before($checkToken);
